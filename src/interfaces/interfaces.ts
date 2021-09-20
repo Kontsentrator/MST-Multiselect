20cda6756d355ab1431b462e611ec6ef4750b01f
Dmitriy <dima.dima-73@yandex.ru>
2021-09-20 09:28:56 +0300

    Create interfaces.ts

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,12 @@
+export interface IItem {
+    id: number,
+    title: string,
+    description: string,
+    specification: string,
+    preview: string,
+    model: string,
+    category_id: number,
+    category: unknown,
+    groud_id: number,
+    group: unknown
+}
