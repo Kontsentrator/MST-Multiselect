e7f14c6474e7f6f1dfa92aebc9c9f623f3c1ae0e
Dmitriy <dima.dima-73@yandex.ru>
2021-09-20 09:28:58 +0300

    Create ListItem.tsx

---
+++ src/ListItem.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {IItem} from './interfaces/interfaces';
+
+type ListItemProps = {
+    item: IItem,
+    checked: boolean
+    onChange(e: React.FormEvent<HTMLInputElement>, id: number): void
+}
+
+const ListItem: React.FC<ListItemProps> = ({item, checked, onChange}) => {
+    return(
+        <li key={item.id} className="item__data">
+            <label>
+                <input type="checkbox" checked={checked} onChange={(e) => onChange(e, item.id)}></input>
+                {item.title}
+            </label>
+        </li>
+    );
+}
+
+export default ListItem;
