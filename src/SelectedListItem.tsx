d5f0ab95b728416390c19450e48e2e6c985a71a7
Dmitriy <dima.dima-73@yandex.ru>
2021-09-20 14:46:03 +0300

    Create SelectedListItem.tsx

---
+++ src/SelectedListItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {IItem} from 'interfaces/interfaces';
+import delete_img from 'img/Clear.svg';
+
+type SelectedListItemProps = {
+    item: IItem,
+    handleDeleteClick(id: number): void
+}
+
+const SelectedListItem: React.FC<SelectedListItemProps> = ({item, handleDeleteClick}) => {
+    return(
+        <li className="selected-item">
+            <span className="selected-item__text">{item.title}</span>
+            <button className="button-clear" onClick={() => handleDeleteClick(item.id)}><img src={delete_img} alt="Убрать" /></button>
+        </li>
+    );
+}
+
+export default SelectedListItem;
