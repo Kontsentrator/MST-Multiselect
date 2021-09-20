4f90efdc48fac656dacc2a3429eadd158b7bf96d
Dmitriy <dima.dima-73@yandex.ru>
2021-09-20 10:22:47 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Multiselect from "./Multiselect";
+import {IItem} from './interfaces/interfaces';
+
+const App: React.FC = () => {
+    const [items, setItems] = useState<IItem[]>([]);
+    // Получение списка по ссылке
+    useEffect(() => {
+        axios.get<IItem[]>("https://api-rguide.admire.social/api/products")
+        .then(function(response) {
+            setItems(response.data);
+        })
+    }, [setItems]);
+
+    return(
+        <Multiselect items={items} />
+    );
+}
+
+export default App;
