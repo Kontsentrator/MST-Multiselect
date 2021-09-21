e3834a08efea57f84d8858432088e731d12569d8
Dmitriy <dima.dima-73@yandex.ru>
2021-09-21 08:46:25 +0300

    Update ListItem.tsx

--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 import { IItem } from 'interfaces/interfaces';
 
 type ListItemProps = {
-    item: IItem,
+    item: IItem | string,
     checked: boolean
     onChange(e: React.FormEvent<HTMLInputElement>, id: number): void
 }
@@ -11,8 +11,13 @@ const ListItem: React.FC<ListItemProps> = ({item, checked, onChange}) => {
     return(
         <li className="item">
             <label className="item__wrap">
-                <input type="checkbox" checked={checked} onChange={(e) => onChange(e, item.id)}></input>
-                {item.title}
+                <input 
+                    type="checkbox" 
+                    checked={checked} 
+                    onChange={(e) => onChange(e, typeof item === "string" ? item.indexOf(item) : item.id)}>
+                </input>
+                
+                {typeof item === "string" ? item : item.title}
             </label>
         </li>
     );
