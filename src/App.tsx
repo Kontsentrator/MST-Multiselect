97f995da5f91fe3f68fabad6802be97c34ba7e3c
Dmitriy <dima.dima-73@yandex.ru>
2021-09-22 10:44:20 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ const App: React.FC = () => {
 
     return(
         <>
-            <Multiselect label_text="Выберите участников" items={["Ануфриенко Н.", "Астафьев П.", "Элемент 3", "Элемент 4", "Элемент 5"]} />
-            <Multiselect label_text="Выберите устройство" items={items} />
+            <Multiselect label_text="Выберите участников" items={["Ануфриенко Н.", "Астафьев П.", "Участник 3", "Участник 4", "Участник 5"]} />
+            <Multiselect label_text="Выберите устройства" items={items} />
         </>
     );
 }
