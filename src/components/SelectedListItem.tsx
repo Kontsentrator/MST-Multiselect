308d69cda8dbdb8030752fd1774fc56ee3f698c0
Dmitriy <dima.dima-73@yandex.ru>
2021-10-12 17:22:54 +0300

    Update SelectedListItem.tsx

--- src/components/SelectedListItem.tsx
+++ src/components/SelectedListItem.tsx
@@ -1,28 +1,37 @@
-import React, { memo } from 'react';
-import { IItem } from 'interfaces/interfaces';
+import React, { memo } from "react";
+import { IItem } from "interfaces/interfaces";
 
-import delete_img from 'img/Clear.svg';
+import delete_img from "img/Clear.svg";
 
 type SelectedListItemProps = {
-    item: IItem | string,
-    handleDeleteClick(id: number): void
-}
+  item: IItem | string;
+  handleDeleteClick(id: number): void;
+};
 
-const SelectedListItem: React.FC<SelectedListItemProps> = ({item, handleDeleteClick}) => {
-    return(
-        <li className="selected-item">
-            <div className="selected-item__wrap">
-                <span className="selected-item__text">
-                    {typeof item === "string" ? item : item.title}
-                </span>
-                
-                <button className="button-clear" 
-                    onClick={() => handleDeleteClick(typeof item === "string" ? item.indexOf(item) : item.id)}>
-                    <img src={delete_img} alt="Убрать" />
-                </button>
-            </div>
-        </li>
-    );
-}
+const SelectedListItem: React.FC<SelectedListItemProps> = ({
+  item,
+  handleDeleteClick,
+}) => {
+  return (
+    <li className="selected-item">
+      <div className="selected-item__wrap">
+        <span className="selected-item__text">
+          {typeof item === "string" ? item : item.title}
+        </span>
+
+        <button
+          className="button-clear"
+          onClick={() =>
+            handleDeleteClick(
+              typeof item === "string" ? item.indexOf(item) : item.id
+            )
+          }
+        >
+          <img src={delete_img} alt="Убрать" />
+        </button>
+      </div>
+    </li>
+  );
+};
 
 export default memo(SelectedListItem);
