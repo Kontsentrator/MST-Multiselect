f30f5512ae363bfa7b0c60d6089f5b269b183a44
Dmitriy <dima.dima-73@yandex.ru>
2021-10-12 17:22:42 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Multiselect from "components/Multiselect";
-import { IItem } from 'interfaces/interfaces';
+import { IItem } from "interfaces/interfaces";
 
 const App: React.FC = () => {
-    const API_ENDPOINT = "https://api-rguide.admire.social/api/products";
-    const [items, setItems] = useState<IItem[] | string[]>([]);
-    
-    // Получение списка по ссылке
-    useEffect(() => {
-        axios.get<IItem[]>(API_ENDPOINT)
-        .then(function(response) {
-            setItems(response.data);
-        })
-    }, [setItems]);
+  const API_ENDPOINT = "https://api-rguide.admire.social/api/products";
+  const [items, setItems] = useState<IItem[] | string[]>([]);
 
-    return(
-        <>
-            <Multiselect labelText="Выберите участников" items={["Ануфриенко Н.", "Астафьев П.", "Участник 3", "Участник 4", "Участник 5"]} />
-            <Multiselect labelText="Выберите устройства" items={items} />
-        </>
-    );
-}
+  // Получение списка по ссылке
+  useEffect(() => {
+    axios.get<IItem[]>(API_ENDPOINT).then(function (response) {
+      setItems(response.data);
+    });
+  }, [setItems]);
+
+  return (
+    <>
+      <Multiselect
+        labelText="Выберите участников"
+        items={[
+          "Ануфриенко Н.",
+          "Астафьев П.",
+          "Участник 3",
+          "Участник 4",
+          "Участник 5",
+        ]}
+      />
+      <Multiselect labelText="Выберите устройства" items={items} />
+    </>
+  );
+};
 
 export default App;
