d268379ada5f0dbc81db34f8b048b718ecb491a5
Dmitriy <dima.dima-73@yandex.ru>
2021-10-12 17:22:46 +0300

    Update ListItem.tsx

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,28 +1,30 @@
-import React, { memo } from 'react';
-import { IItem } from 'interfaces/interfaces';
+import React, { memo } from "react";
+import { IItem } from "interfaces/interfaces";
 
 type ListItemProps = {
-    item: IItem | string,
-    checked: boolean
-    onChange(e: React.FormEvent<HTMLInputElement>, id: number): void
-}
+  item: IItem | string;
+  checked: boolean;
+  onChange(e: React.FormEvent<HTMLInputElement>, id: number): void;
+};
 
-const ListItem: React.FC<ListItemProps> = ({item, checked, onChange}) => {
-    return(
-        <li className="item">
-            <label className="item__wrap">
-                <input 
-                    type="checkbox" 
-                    className="checkbox"
-                    checked={checked} 
-                    onChange={(e) => onChange(e, typeof item === "string" ? item.indexOf(item) : item.id)}
-                />
-                <label />
-                
-                {typeof item === "string" ? item : item.title}
-            </label>
-        </li>
-    );
-}
+const ListItem: React.FC<ListItemProps> = ({ item, checked, onChange }) => {
+  return (
+    <li className="item">
+      <label className="item__wrap">
+        <input
+          type="checkbox"
+          className="checkbox"
+          checked={checked}
+          onChange={(e) =>
+            onChange(e, typeof item === "string" ? item.indexOf(item) : item.id)
+          }
+        />
+        <span />
+
+        {typeof item === "string" ? item : item.title}
+      </label>
+    </li>
+  );
+};
 
 export default memo(ListItem);
