f93cbe98804a7bf735ea78b50fbd32514356df63
Dmitriy <dima.dima-73@yandex.ru>
2021-10-14 11:18:57 +0300

    Delete Multiselect.test.js

--- src/Multiselect.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Multiselect from './Multiselect';
-
-test('renders learn react link', () => {
-  render(<Multiselect />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
