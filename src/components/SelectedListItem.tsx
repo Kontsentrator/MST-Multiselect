0bd0cf2c42bf855257f9d34804646a2292747684
Dmitriy <dima.dima-73@yandex.ru>
2021-10-18 14:34:22 +0300

    Update SelectedListItem.tsx
    Оптимизация компонента для работы с типом string

--- src/components/SelectedListItem.tsx
+++ src/components/SelectedListItem.tsx
@@ -1,10 +1,7 @@
 import React, { memo } from "react";
-import { IItem } from "interfaces/interfaces";
-
-import delete_img from "img/Clear.svg";
 
 type SelectedListItemProps = {
-  item: IItem | string;
+  item: string;
   handleDeleteClick(id: number): void;
 };
 
@@ -16,19 +13,17 @@ const SelectedListItem: React.FC<SelectedListItemProps> = ({
     <li className="selected-item">
       <div className="selected-item__wrap">
         <span className="selected-item__text">
-          {typeof item === "string" ? item : item.title}
+          {item}
         </span>
 
         <button
           className="button-clear"
           onClick={() =>
             handleDeleteClick(
-              typeof item === "string" ? item.indexOf(item) : item.id
+              item.indexOf(item)
             )
           }
-        >
-          <img src={delete_img} alt="Убрать" />
-        </button>
+        />
       </div>
     </li>
   );
